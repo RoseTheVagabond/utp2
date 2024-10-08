4f76df8c9183555aed14c7ea185f2aa63bfe76df
RoseTheVagabond <rosethevagabond@gmail.com>
2024-10-08 09:36:02 +0200

    Changed adder parameters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
+        System.out.println(adder.add(5, 4));
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(19, 7));
     }
